b8df9a1f9e57bb72b7e01e4400208153c3fddcac
Maksin Spizhuk <indcheat@mail.ru>
2025-03-19 11:51:55 +0300

    adeed more details

---
+++ functionals/script.js
@@ -0,0 +1,7 @@
+function dataListEach() {
+  const list = document.getElementsByClassName("main__section-1")[0];
+  console.log(list);
+  list.forEach((element) => {
+    console.log(element);
+  });
+}
